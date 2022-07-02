9aaa4c14561deab9e88a5d12886af86af49beae4
Hung Do <hungdojan@gmail.com>
2022-07-02 17:18:27 +0200

    Add Font Awesome

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from sys import argv
 from sort_lib.sort import Sort
 from gui_lib.g_main_window import GMainWindow
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QFontDatabase
 
 def main():
     app = QApplication(argv)
+
+    # nacteni fontu
+    QFontDatabase.addApplicationFont(":/font-awesome-brands.otf")
+    QFontDatabase.addApplicationFont(":/font-awesome-regular.otf")
+    QFontDatabase.addApplicationFont(":/font-awesome-solid.otf")
 
     g_main_window = GMainWindow(Sort())
     g_main_window.show()
